73bb8af65d116a3fdd9440bfe1390908bbc0255d
Yosua Marchel <marchel.yosua@gmail.com>
2023-10-06 18:07:00 +0700

    push hasil belajar nodejs npm

---
+++ package.js
@@ -0,0 +1,9 @@
+import { sayHello, sum } from "yosuamarchel92-belajar-nodejs-npm-library";
+import { min, max } from "yosuamarchel92-belajar-nodejs-npm-library/number";
+
+console.info(sayHello("Eko"));
+
+console.info(sum([10,10,10,10,10]));
+
+console.info(min(10,20));
+console.info(max(10,20));
